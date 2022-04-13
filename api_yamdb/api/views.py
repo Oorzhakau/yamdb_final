7678f198b5c39ce93de0284de29263f07220ffb2
Alexander <oorzhakau@yandex.ru>
2022-04-13 23:37:05 +0300

    isort error

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -15,7 +15,6 @@ from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import RefreshToken
-
 from reviews.models import Category, Genre, Title
 
 from .filters import TitleFilter
